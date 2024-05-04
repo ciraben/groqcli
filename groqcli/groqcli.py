4dc144013c5d471b377c8b9fedc13f9b09a3497f
ciraben <tom.on.github@gmail.com>
2024-05-03 22:13:13 -0700

    Add optional args to provide greater control over responses. Make function description more descriptive.

--- groqcli/groqcli.py
+++ groqcli/groqcli.py
@@ -1,16 +1,29 @@
 import os
 from groq import Groq
 
-def complete(content: String = "Explain the importance of fast language models"):
-    """Print some content's completion to the terminal.
+def get_completion(
+        content: str = "Explain the importance of fast language models",
+        prompt: str = "you are a helpful assistant.",
+        model: str = "mixtral-8x7b-32768",
+        max_tokens: int = 50,
+        temperature: float = 1
+    ):
 
-    Extended description of function.
+    """Return chat completion of input.
+
+    Given some user text `content` as input, generate a chat completion using
+    the Groq API and return it. Optionally specify which model to use, as well
+    as how it should respond.
 
     Args:
         content (str): Some text to be passed to an LLM for completion.
+        prompt (str): A text prompt to specify how the model should respond.
+        model (str): The name of the model to request chat completion from.
+        max_tokens (int): The maximum number of tokens the model's response may contain.
+        temperature (float): The randomness of responses - larger values are more random. Accepts values > 0 and <= 2.
 
     Returns:
-        None
+        completion (str): The model's chat completion response as text.
     """
 
     client = Groq(
@@ -19,14 +32,19 @@ def complete(content: String = "Explain the importance of fast language models")
     chat_completion = client.chat.completions.create(
         messages=[
             {
+                "role": "system",
+                "content": prompt
+            },
+            {
                 "role": "user",
-                "content": query,
+                "content": content,
             }
         ],
-        model="mixtral-8x7b-32768",
+        model=model,
+        max_tokens=max_tokens
     )
-    print(chat_completion.choices[0].message.content)
-    return
+    return chat_completion.choices[0].message.content
 
 if __name__ == "__main__":
-    complete()
+    completion = get_completion()
+    print(completion)
